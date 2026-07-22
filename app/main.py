5071313bbbbc8c45ffeeaa1a6602faf4f6ae402d
Nicole Liu <nicole.liu.sf@gmail.com>
2026-07-22 12:14:23 -0700

    Add prompt management system

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from llm import ask_llm
+from prompts import load_research_prompt
 
 
 question = "What are the benefits of resistance training for older adults?"
 
 
-answer = ask_llm(question)
+prompt = load_research_prompt(question)
+
+
+answer = ask_llm(prompt)
 
 
 print("Question:")
